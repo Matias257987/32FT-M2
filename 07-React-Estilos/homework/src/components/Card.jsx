9bc5ba72382fad3d796b803663042d45f95324dc
Matias <acostamatias25@gmail.com>
2022-11-01 21:36:00 -0300

    React-Styles

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import Style from '../styles/Card.module.css';
 
-export default function Card(props) {
+export default function Card({max, min, name, img, onClose}) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+  <div class={Style.container}>
+    <button id={Style.close} onClick={onClose}> X </button>
+    <h3 id={Style.name}> {name} </h3>
+    <div class={Style.info}>
+      <span >min: {min}</span>
+      <span >max: {max}</span>
+    </div>
+    <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="imagen"/>
+  </div>);
 };
--- 07-React-Estilos/homework/src/components/SearchBar.jsx
+++ 07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import Style from '../styles/SearchBar.module.css'
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return <div>
+    <input type="text" name="search" id={Style.search} placeholder='Busca tu Ciudad ...' />
+    <button id={Style.buscar} onClick={() => props.onSearch()}> Agregar </button>
+  </div>
 };
